a24b388d5fe5d15b892eeb0dca6416ddd77d999e
lilylagrange00 <100016480+lilylagrange00@users.noreply.github.com>
2026-02-24 17:39:09 -0600

    Initialize Firebase app only if service account exists

--- server/middleware/init.ts
+++ server/middleware/init.ts
@@ -2,11 +2,14 @@ import admin from "firebase-admin";
 
 export default defineEventHandler(() => {
   const config = useRuntimeConfig();
-  const decodedServiceAccount = Buffer.from(config.base64ServiceAccount, "base64").toString("utf-8");
-  const credentials = JSON.parse(decodedServiceAccount);
 
-  if (!admin.apps.length)
-    admin.initializeApp({
-      credential: admin.credential.cert(credentials)
-    });
+  if (config.base64ServiceAccount) {  // Only run if the env var exists
+    const decodedServiceAccount = Buffer.from(config.base64ServiceAccount, "base64").toString("utf-8");
+    const credentials = JSON.parse(decodedServiceAccount);
+
+    if (!admin.apps.length)
+      admin.initializeApp({
+        credential: admin.credential.cert(credentials)
+      });
+  }
 });
